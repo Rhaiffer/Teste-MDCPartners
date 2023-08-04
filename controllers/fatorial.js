a37415b28049f16d3861a4c700b9012c9768d85a
Rhaiffer <86625812+Rhaiffer@users.noreply.github.com>
2023-08-04 11:07:53 -0300

    Teste back-end para MDC Partners

--- controllers/fatorial.js
+++ controllers/fatorial.js
@@ -1,28 +1,37 @@
 // Função para calcular o fatorial de um número
 function calcularFatorial(numero) {
-  if (numero === 0 || numero === 1) {
-    return 1; // Casos base: o fatorial de 0 e 1 é 1
+  if (numero === 0n || numero === 1n) {
+    return 1n; // Casos base: o fatorial de 0 e 1 é 1
   } else {
-    return numero * calcularFatorial(numero - 1); // Caso geral: calcula o fatorial de forma recursiva
+    return numero * calcularFatorial(numero - 1n); // Caso geral: calcula o fatorial de forma recursiva
   }
+}
+
+// Converte o resultado do fatorial para notação científica
+function formatarNotacaoCientifica(fatorial) {
+  const fatorialStr = fatorial.toString();
+  const mantissa = fatorialStr.slice(0, 1) + '.' + fatorialStr.slice(1, 16);
+  const expoente = fatorialStr.length - 1;
+  return mantissa + 'e' + expoente;
 }
 
 // Programa para obter o número do usuário e calcular o fatorial
 const readline = require('readline').createInterface({
-  input: process.stdin,  // Define a entrada padrão como o console (stdin)
-  output: process.stdout // Define a saída padrão como o console (stdout)
+  input: process.stdin,
+  output: process.stdout
 });
 
 readline.question('Digite um número para calcular o fatorial: ', (numero) => {
   // Verificar se o usuário inseriu um número válido
-  numero = parseInt(numero); // Converte a entrada em um número inteiro
+  numero = BigInt(numero); // Converte a entrada para BigInt
 
-  if (isNaN(numero) || numero < 0) {
-    console.log('Por favor, insira um número inteiro positivo.'); // Exibe mensagem de erro para entrada inválida
+  if (Number.isNaN(Number(numero)) || numero < 0n) {
+    console.log('Por favor, insira um número inteiro positivo.');
   } else {
-    const fatorial = calcularFatorial(numero); // Chama a função para calcular o fatorial do número
-    console.log(`O fatorial de ${numero} é: ${fatorial}`); // Exibe o resultado do fatorial
+    const fatorial = calcularFatorial(numero);
+    const resultadoFormatado = formatarNotacaoCientifica(fatorial);
+    console.log(`O fatorial de ${numero} é: ${resultadoFormatado}`);
   }
 
-  readline.close(); // Fecha a interface de leitura de linha para permitir que o programa termine corretamente
+  readline.close();
 });
